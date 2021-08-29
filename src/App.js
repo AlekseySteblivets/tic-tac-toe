6af5c6ee926bbef7f5d014e7fea41f7cf1be7034
Aleksey Steblivets <aleksey.steblivets@gmail.com>
2021-08-29 21:24:53 +0300

    start adding Game component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import Game from './components/Game/Game';
+// import Game from './components/Game/Game';
+import Board from './components/Board/Board';
 import './App.css';
 
 // console.log(Game);
 function App() {
   return (
     <div className="App">
-      <Game />
+      <Board />
     </div>
   );
 }
--- src/components/Square/Square.js
+++ src/components/Square/Square.js
@@ -1,20 +1,28 @@
 import React from "react";
 
-class Square extends React.Component {
-    // state = {
-    //     value: null,
-    // }
-
-    render() {
-        return (
-            <button
-                className="square"
-                onClick={() => this.props.onClick()}
-            >
-                {this.props.value}
-            </button>
-        );
-    }
+function Square({ value, onClick }) {
+    return (
+        <button className="square" onClick={onClick}>
+            {value}
+        </button>
+    )
 }
+
+// class Square extends React.Component {
+//     // state = {
+//     //     value: null,
+//     // }
+
+//     render() {
+//         return (
+//             <button
+//                 className="square"
+//                 onClick={() => this.props.onClick()}
+//             >
+//                 {this.props.value}
+//             </button>
+//         );
+//     }
+// }
 
 export default Square;
